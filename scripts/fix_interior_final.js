d0d2ca8db374b2c84d688bdb4712c426ad7f9202
Pineforge Digital <pineforgedigital@gmail.com>
2026-08-31 20:11:29 -0500

    chore: force vercel cache bust 3 for interior image update

--- scripts/fix_interior_final.js
+++ scripts/fix_interior_final.js
@@ -11,7 +11,7 @@ const client = createClient({
 });
 
 async function main() {
-  const filePath = path.join(__dirname, '../Business photos/Decorating/IMG_0804.jpg');
+  const filePath = path.join(__dirname, '../Business photos/Kitchens/IMG_2760.jpg');
   const filename = path.basename(filePath);
   console.log(`Uploading ${filename}...`);
   
