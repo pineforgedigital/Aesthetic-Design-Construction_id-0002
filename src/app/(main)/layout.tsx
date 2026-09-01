2761c1b75dfc7dcca49baf5d89e3800d7f52a8a0
Pineforge Digital <pineforgedigital@gmail.com>
2026-08-31 20:00:22 -0500

    style: restore decorative leaves to layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,13 @@ export default function MainLayout({
 }>) {
   return (
     <>
-
+      {/* Decorative Background Leaves */}
+      <div className="fixed -left-20 md:-left-32 top-[20%] z-0 opacity-20 md:opacity-40 mix-blend-multiply pointer-events-none w-48 md:w-auto">
+        <Image src="/leaf.jpg" alt="" width={400} height={400} className="object-contain -rotate-12" priority />
+      </div>
+      <div className="fixed -right-24 md:-right-32 bottom-[10%] z-0 opacity-15 md:opacity-30 mix-blend-multiply pointer-events-none w-64 md:w-auto">
+        <Image src="/leaf.jpg" alt="" width={500} height={500} className="object-contain rotate-180" priority />
+      </div>
 
       <Navbar />
       <div className="flex-grow pt-[112px] md:pt-[128px]">
