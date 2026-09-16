3632e6c65b7b6d1450c649073c9b449993bf3eb7
Pineforge Digital <pineforgedigital@gmail.com>
2026-09-16 18:52:51 -0500

    Speed up smooth scrolling duration to reduce perceived lag

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -10,7 +10,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.6,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       wheelMultiplier: 1,
       touchMultiplier: 2,
